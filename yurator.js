4392912a9015039342d4d377415d6851cca536dc
Igor Correa <igorbuenocorrea@gmail.com>
2015-08-30 12:57:54 -0300

    Frases prontas
    Adicionadas algumas frases prontas a serem sorteadas 15% das vezes.
    Também foram feitas alterações no “estilo” do código que não mudam nada
    no funcionamento da parada.

--- yurator.js
+++ yurator.js
@@ -17,48 +17,60 @@ function Yuri() {
 		Array("o ", "seu ", "transsexualismo", "transsexual"),
 		Array("o ", "seu ", "feminismo", "feminista"),
 		Array("a ", "sua ", "felicidade", "feliz"),
-		Array("a ", "sua ", "vulnerabilidade", "vulner&aacutevel")
+		Array("a ", "sua ", "vulnerabilidade", "vulnerável")
 	];
 
 	this.temas = [
 		Array("a ", "sua ", "homofobia", "homofóbico"),
 		Array("a ", "sua ", "misoginia", "misógino"),
-		Array("a ", "sua ", "xenofobia", "xenof&oacutebico"),
+		Array("a ", "sua ", "xenofobia", "xenofóbico"),
 		Array("o ", "seu ", "transsexualismo", "transsexual"),
 		Array("o ", "seu ", "feminismo", "feminista"),
 		Array("a ", "sua ", "felicidade", "feliz"),
-		Array("a ", "sua ", "vulnerabilidade", "vulner&aacutevel"),
+		Array("a ", "sua ", "vulnerabilidade", "vulnerável"),
 		Array("o ", "seu ", "machismo", "machista"),
-		Array("o ", "seu ", "ego&iacutesmo", "ego&iacutesta"),
-		Array("o ", "seu ", "altru&iacutesmo", "altru&iacutesta"),
+		Array("o ", "seu ", "egoísmo", "egoísta"),
+		Array("o ", "seu ", "altruísmo", "altruísta"),
 		Array("a ", "sua ", "liberdade", "livre"),
 		Array("o ", "seu ", "preconceito", "preconceituoso")
 	];
 
 	this.temasPositivos = [
-		Array("o ", "seu ", "altru&iacutesmo", "altru&iacutesta"),
+		Array("o ", "seu ", "altruísmo", "altruísta"),
 		Array("a ", "sua ", "liberdade", "livre"),
 		Array("o ", "seu ", "feminismo", "feminista"),
 		Array("a ", "sua ", "\"interessantisse\"", "interessante"),
 		Array("a ", "sua ", "felicidade", "feliz"),
-		Array("a ", "sua ", "vulnerabilidade", "vulner&aacutevel")
+		Array("a ", "sua ", "vulnerabilidade", "vulnerável")
 	];
 
 	this.temasNegativos = [
-		Array("o ", "seu ", "ego&iacutesmo", "ego&iacutesta"),
-		Array("a ", "sua ", "homofobia", "homof&oacutebico"),
-		Array("a ", "sua ", "misoginia", "mis&oacutegino"),
-		Array("a ", "sua ", "xenofobia", "xenof&oacutebico"),
+		Array("o ", "seu ", "egoísmo", "egoísta"),
+		Array("a ", "sua ", "homofobia", "homofóbico"),
+		Array("a ", "sua ", "misoginia", "misógino"),
+		Array("a ", "sua ", "xenofobia", "xenofóbico"),
 		Array("o ", "seu ", "machismo", "machista"),
 		Array("o ", "seu ", "preconceito", "preconceituoso")
 	];
 
 	this.temasPolemicos = [
 		Array("a ", "sua ", "liberdade", "livre"),
-		Array("o ", "seu ", "altru&iacutesmo", "altru&iacutesta"),
+		Array("o ", "seu ", "altruísmo", "altruísta"),
 		Array("o ", "seu ", "transexualismo", "transsexual"),
+		Array("a ", "sua ", "vulnerabilidade", "vulnerável"),
 		Array("a ", "sua ", "felicidade", "feliz", "machista")
 	];
+
+	this.frasesProntas = [
+		"Fifa 16 tem times femininos. Fuck Yeah!",
+		"Acho que vou doar meu rim.",
+		"Se você falar pra alguém te estuprar ainda é estupro?",
+		"O que acontece se voce tocar a Song of Time dentro da Lua?",
+		"Tô pensando se tento virar modelo de barba.",
+		"Pão de queijo não se compara. Pão de queijo se come.",
+		"Alguém topa um café?",
+		"Ser cuzão privilegiado ou hipócrita privilegiado?"
+		];
 
 	this.frases = [
 		Array("@1 @2 > @3 @4?",
@@ -66,25 +78,25 @@ function Yuri() {
 				  this.temasPositivos, tipoTemaEnum.SUB,
 				  this.temasPositivos, tipoTemaEnum.SIMPLES,
 				  this.temasNegativos, tipoTemaEnum.SUB)),
-		Array("O que &eacute pior, ser @1 ou ser @2?",
+		Array("O que é pior, ser @1 ou ser @2?",
 			Array(this.temasNegativos, tipoTemaEnum.SUB,
 				  this.temasNegativos, tipoTemaEnum.SUB)),
-		Array("O que voc&ecircs prefeririam, ser @1 ou ser @2?",
+		Array("O que vocês prefeririam, ser @1 ou ser @2?",
 			Array(this.temas, tipoTemaEnum.SUB,
 				  this.temas, tipoTemaEnum.SUB)),
-		Array("Gente, voc&ecircs preferem uma vida @1 ou uma vida @2?",
+		Array("Gente, vocês preferem uma vida @1 ou uma vida @2?",
 			Array(this.temasPositivos, tipoTemaEnum.SUB,
 				  this.temasPositivos, tipoTemaEnum.SUB)),
-		Array("Voc&ecirc trocaria @1 por @2?",
+		Array("Você trocaria @1 por @2?",
 			Array(this.temasPositivos, tipoTemaEnum.POS,
 				  this.temasPositivos, tipoTemaEnum.POS)),
-	    Array("Qual voc&ecircs preferem? A: @1. B: @2.",
+	    Array("Qual vocês preferem? A: @1. B: @2.",
 	    	Array(this.temasNeutros, tipoTemaEnum.POS,
 				  this.temasNeutros, tipoTemaEnum.POS)),
-	    Array("Voc&ecirc abriria m&atildeo de @1 para acabar com @2?",
+	    Array("Você abriria mão de @1 para acabar com @2?",
 	    	Array(this.temasPositivos, tipoTemaEnum.POS,
 				  this.temasNegativos, tipoTemaEnum.ART)),
-	    Array("@1 &eacute uma quest&atildeo pol&ecircmica, n&eacute?",
+	    Array("@1 é uma questão polêmica.",
 	    	Array(this.temasPolemicos, tipoTemaEnum.SIMPLES))
   	];
 }
@@ -94,37 +106,49 @@ Yuri.prototype.getRandomElement = function (vec) {
 }
 
 Yuri.prototype.getTema = function(tipo, tema) {
-	if (tipo == tipoTemaEnum.ART)
+	
+	switch(tipo) {
+		case tipoTemaEnum.ART:
 		return tema[0] + tema[2];
-
-	else if (tipo == tipoTemaEnum.ART_POS)
+		
+		case tipoTemaEnum.ART_POS:
 		return tema[0] + tema[1] + tema[2];
-
-	else if (tipo == tipoTemaEnum.POS)
+		
+		case tipoTemaEnum.POS:
 		return tema[1] + tema[2];
-
-	else if (tipo == tipoTemaEnum.SIMPLES)
+		
+		case tipoTemaEnum.SIMPLES:
 		return tema[2];
-
-	else if (tipo == tipoTemaEnum.SUB)
+		
+		case tipoTemaEnum.SUB:
 		return tema[3];
+	}
 }
 
 Yuri.prototype.yurar = function() {
 	var tema;
 	var i;
 	var j;
-	var fraseEDados = this.getRandomElement(this.frases);
-
-	var frase = fraseEDados[0];
-	var dadosSubstituir = fraseEDados[1];
-
-	for(i = 0, j = 1; i < dadosSubstituir.length; i = i + 2, j++) {
-		tema = this.getRandomElement(dadosSubstituir[i]);
-
-		frase = frase.replace("@" + j, this.getTema(dadosSubstituir[i+1], tema))
+	var frase;
+	var fraseEDados;
+	var dadosSubstituir; 
+	
+	if (Math.random() < .15) {
+		frase = this.getRandomElement(this.frasesProntas);
 	}
-
+	else {
+		fraseEDados = this.getRandomElement(this.frases);
+	
+		frase = fraseEDados[0];
+		dadosSubstituir = fraseEDados[1];
+	
+		for(i = 0, j = 1; i < dadosSubstituir.length; i = i + 2, j++) {
+			tema = this.getRandomElement(dadosSubstituir[i]);
+	
+			frase = frase.replace("@" + j, this.getTema(dadosSubstituir[i+1], tema))
+		}	
+	}
+	
 	frase = frase.capitalizeFirstLetter();
 
 	return frase;
